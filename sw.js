60d51a7cf99a16815920adef9c9b4c51f5c40f4e
chopper-coder <doulioufire119@gmail.com>
2026-06-14 19:57:37 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aed-pwa-all-speak-editable-manifest-fixed-v1';
+const CACHE_NAME = 'aed-pwa-mobile-charge-sound-fixed-v1';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE_NAME ? null : caches.delete(k)))).then(() => self.clients.claim())); });
