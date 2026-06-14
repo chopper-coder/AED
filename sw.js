894b3c7ad3c5804c3c12d9a670faeee455abffe2
chopper-coder <doulioufire119@gmail.com>
2026-06-14 17:05:13 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'aed-pwa-voice-prompt-editor-v1';
+const CACHE_NAME = 'aed-pwa-voice-prompt-editor-flat-v1';
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
